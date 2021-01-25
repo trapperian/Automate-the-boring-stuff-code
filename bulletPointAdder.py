9ee4396b2707542ab2686dcd1033bb93cc966dd1
Ian <44474914+trapperian@users.noreply.github.com>
2021-01-25 05:36:05 -0700

    Tidying code and rejoining new lines to clipboard

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -1,14 +1,13 @@
 #! python3
-#bulletPointAdder.py - Adds wikipedia bullet points to the start of each line of text on clipboard
+# bulletPointAdder.py - Adds wikipedia bullet points to the start of each line of text on clipboard
 
 import pyperclip
+
 text = pyperclip.paste()
 
-#TODO: separate lines and add stars.
-
 # Separate lines and add stars.
-lines = text.split('\n')   #splitting lines on clipboard
-for i in range(len(lines)):  #looping through each of the splitlines
-    lines[i] = '* ' + lines[i] #adding star and space to each line
-
+lines = text.split("\n")  # splitting lines on clipboard
+for i in range(len(lines)):  # looping through each of the splitlines
+    lines[i] = "* " + lines[i]  # adding star and space to each line
+text = "\n".join(lines)  # turning lines into one string for copy function
 pyperclcip.copy(text)
