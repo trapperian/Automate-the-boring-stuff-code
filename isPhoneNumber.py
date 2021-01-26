43f947cc70e0f4b1bf9126647a5bfcbac851dac5
Ian <44474914+trapperian@users.noreply.github.com>
2021-01-26 05:37:31 -0700

    Update isPhoneNumber.py
    Tidy button on Mu for the code

--- isPhoneNumber.py
+++ isPhoneNumber.py
@@ -4,21 +4,22 @@ def isPhoneNumber(text):
     for i in range(0, 3):
         if not text[i].isdecimal():
             return False
-    if text[3] != '-':
+    if text[3] != "-":
         return False
     for i in range(4, 7):
         if not text[i].isdecimal():
             return False
-    if text[7] != '-':
+    if text[7] != "-":
         return False
     for i in range(8, 12):
         if not text[i].isdecimal():
             return False
     return True
 
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+
+message = "Call me at 415-555-1011 tomorrow. 415-555-9999 is my office."
 for i in range(len(message)):
-    chunk = message[i:i+12]
+    chunk = message[i : i + 12]
     if isPhoneNumber(chunk):
-        print('Phone number found: ' + chunk)
-print('Done')
+        print("Phone number found: " + chunk)
+print("Done")
