33ac564b6074acbd38c38ee9ad2a854ed14df5a4
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-16 06:24:33 -0700

    Update searchPypi.py

--- searchPypi.py
+++ searchPypi.py
@@ -5,3 +5,10 @@ import requests
 import sys
 import webbrowswer
 import bs4
+
+print("Searching...")  # display text while downloading the search result page
+res = requests.get(
+    "https://google.com/search?q="
+    "https://pypi.org/search/?q=" + " ".join(sys.argv[1:])
+)
+res.raise_for_status()
