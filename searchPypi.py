e894ae2890a1b7e7f1eb8802ee463d25a5329881
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-16 06:30:01 -0700

    Complete open tab functionality

--- searchPypi.py
+++ searchPypi.py
@@ -17,3 +17,8 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text, "html.parser")
 # open a browser tab for each result.
 linkElems = soup.select(".package-snippet")
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
