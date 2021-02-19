dcffd7c9bbabb4a18ca75e7f5510867b2985a5a8
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-19 07:03:29 -0700

    Create firstOpenPyxl.py

---
+++ firstOpenPyxl.py
@@ -0,0 +1,12 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('example.xlsx')
+print(wb.sheetnames)
+
+sheet = wb['Sheet3']
+type(sheet)
+
+print(sheet.title)
+
+anotherSheet = wb.active
+type(anotherSheet)
