859fbcb06fb5e9c189b0aeeaae02e2e823570308
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-19 11:26:58 -0700

    Tidy funtion on mu

--- firstOpenPyxl.py
+++ firstOpenPyxl.py
@@ -1,9 +1,9 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('example.xlsx')
+wb = openpyxl.load_workbook("example.xlsx")
 print(wb.sheetnames)
 
-sheet = wb['Sheet3']
+sheet = wb["Sheet3"]
 type(sheet)
 
 print(sheet.title)
