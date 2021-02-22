831279013bc4bd1f6c63ad1a9d79ebdebbe33e8d
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-22 05:43:15 -0700

    Deleting Mu File Code Saved as HTML From REPL in Mu

--- firstOpenPyxl.py
+++ firstOpenPyxl.py
@@ -1,12 +1,1 @@
-import openpyxl
 
-wb = openpyxl.load_workbook("example.xlsx")
-print(wb.sheetnames)
-
-sheet = wb["Sheet3"]
-type(sheet)
-
-print(sheet.title)
-
-anotherSheet = wb.active
-type(anotherSheet)
