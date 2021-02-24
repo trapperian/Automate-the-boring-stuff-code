28c3286027354c4a4d7db186d2d63b396086f61a
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-24 05:38:08 -0700

    Update readCensusExcel.py

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -8,3 +8,9 @@ wb = openpyxl.load_workbook('censuspopdata.xlsx')
 sheet = wb['Population by Census Tract']
 countyData = {}
 
+print('Reading rows...')
+for row in range(2, sheet.max_row + 1):
+    #each row in the spreadsheet has data for one census tract.
+    state = sheet['B' + str(row)].value
+    county = sheet['C' + str(row)].value
+    pop = sheet['D' + str(row)].value
