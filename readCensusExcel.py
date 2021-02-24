7b738f011a83763a3fe6cb6c3bb4b810d6bbece5
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-24 05:59:13 -0700

    Tidy function on Mu editor

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -3,25 +3,25 @@
 
 import openpyxl
 import pprint
-print('Opening workbooks...')
-wb = openpyxl.load_workbook('censuspopdata.xlsx')
-sheet = wb['Population by Census Tract']
+
+print("Opening workbooks...")
+wb = openpyxl.load_workbook("censuspopdata.xlsx")
+sheet = wb["Population by Census Tract"]
 countyData = {}
 
-print('Reading rows...')
+print("Reading rows...")
 for row in range(2, sheet.max_row + 1):
-    #each row in the spreadsheet has data for one census tract.
-    state = sheet['B' + str(row)].value
-    county = sheet['C' + str(row)].value
-    pop = sheet['D' + str(row)].value
+    # each row in the spreadsheet has data for one census tract.
+    state = sheet["B" + str(row)].value
+    county = sheet["C" + str(row)].value
+    pop = sheet["D" + str(row)].value
 
-    #check whether key for this state exists.
+    # check whether key for this state exists.
     countyData.setdefault(state, {})
     # check whether key for this county in this state exists.
-    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+    countyData[state].setdefault(county, {"tracts": 0, "pop": 0})
 
     # each row represents one census tract, so increment by one.
-    countyData[sttate][county]['tracts'] += 1
+    countyData[sttate][county]["tracts"] += 1
     # increases the county pop by the pop in the current census tract.
-    countyData[state][county]['pop'] += int(pop)
-
+    countyData[state][county]["pop"] += int(pop)
