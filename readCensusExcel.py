f244398c5822283d148ef46470824dfe5f650072
Ian <44474914+trapperian@users.noreply.github.com>
2021-02-24 05:58:54 -0700

    Update readCensusExcel.py

--- readCensusExcel.py
+++ readCensusExcel.py
@@ -14,3 +14,14 @@ for row in range(2, sheet.max_row + 1):
     state = sheet['B' + str(row)].value
     county = sheet['C' + str(row)].value
     pop = sheet['D' + str(row)].value
+
+    #check whether key for this state exists.
+    countyData.setdefault(state, {})
+    # check whether key for this county in this state exists.
+    countyData[state].setdefault(county, {'tracts': 0, 'pop': 0})
+
+    # each row represents one census tract, so increment by one.
+    countyData[sttate][county]['tracts'] += 1
+    # increases the county pop by the pop in the current census tract.
+    countyData[state][county]['pop'] += int(pop)
+
